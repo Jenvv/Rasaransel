05c45a987f87caebc3731b813ecba2fca2bb4223
Ailani1111 <layu318228@gmail.com>
2024-06-01 17:47:56 +0700

    pelayanan pelanggan

--- application/views/Pelanggan/Auth/login.php
+++ application/views/Pelanggan/Auth/login.php
@@ -1,3 +1,10 @@
+<!-- Title Page -->
+<section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15"
+    style="background-image: url(<?= base_url('asset/foto2.jpg') ?>);">
+    <h2 class="tit6 t-center">
+        Login
+    </h2>
+</section>
 <!-- Reservation -->
 <section class="section-reservation bg1-pattern p-t-100 p-b-113">
     <div class="container">
