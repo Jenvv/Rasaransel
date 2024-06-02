cd57fef75796b833476f4aa6fcbcec85726d9039
Ailani1111 <layu318228@gmail.com>
2024-06-02 22:44:54 +0700

    adminnn

--- application/views/Pemilik/Transaksi/transaksi.php
+++ application/views/Pemilik/Transaksi/transaksi.php
@@ -2,7 +2,7 @@
     <div class="container-fluid p-0">
         <div class="row mb-2 mb-xl-3">
             <div class="col-auto d-none d-sm-block">
-                <h3><strong>Transaksi</strong> User</h3>
+                <h3><strong>Monitoring</strong> User</h3>
             </div>
 
             <div class="col-auto ml-auto text-right mt-n1">
@@ -18,17 +18,17 @@
         <?php
         if ($this->session->userdata('success')) {
         ?>
-            <div class="alert alert-success alert-dismissible" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <div class="alert-icon">
-                    <i class="far fa-fw fa-bell"></i>
-                </div>
-                <div class="alert-message">
-                    <strong>Hallo!!!</strong> <?= $this->session->userdata('success') ?>
-                </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="alert-icon">
+                <i class="far fa-fw fa-bell"></i>
             </div>
+            <div class="alert-message">
+                <strong>Hallo!!!</strong> <?= $this->session->userdata('success') ?>
+            </div>
+        </div>
         <?php
         }
         ?>
@@ -56,12 +56,12 @@
                                 $no = 1;
                                 foreach ($user as $key => $value) {
                                 ?>
-                                    <tr>
-                                        <td><?= $no++ ?></td>
-                                        <td><?= $value->username ?></td>
-                                        <td><?= $value->no_hp ?></td>
-                                        <td><?= $value->alamat ?></td>
-                                        <?php
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $value->username ?></td>
+                                    <td><?= $value->no_hp ?></td>
+                                    <td><?= $value->alamat ?></td>
+                                    <?php
                                         $count_masuk = 0;
                                         foreach ($notif['pesanan_masuk'] as $us) :
                                             if ($us->id_user == $value->id_user) :
@@ -69,7 +69,7 @@
                                             endif;
                                         endforeach;
                                         ?>
-                                        <?php
+                                    <?php
                                         $count_proses = 0;
                                         foreach ($notif['diproses'] as $us) :
                                             if ($us->id_user == $value->id_user) :
@@ -77,7 +77,7 @@
                                             endif;
                                         endforeach;
                                         ?>
-                                        <?php
+                                    <?php
                                         $count_konfirmasi = 0;
                                         foreach ($notif['konfirmasi'] as $us) :
                                             if ($us->id_user == $value->id_user) :
@@ -85,7 +85,7 @@
                                             endif;
                                         endforeach;
                                         ?>
-                                        <?php
+                                    <?php
                                         $count = 0;
                                         foreach ($notif['selesai'] as $us) :
                                             if ($us->id_user == $value->id_user) :
@@ -93,28 +93,34 @@
                                             endif;
                                         endforeach;
                                         ?>
-                                        <td>
-                                            <button class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Pesanan Masuk: <?= $count_masuk ?>">
-                                                <?= $count_masuk ?>
-                                            </button>
-                                            <button class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Konfirmasi: <?= $count_konfirmasi ?>">
-                                                <?= $count_konfirmasi ?>
-                                            </button>
-                                            <button class="btn btn-info" data-toggle="tooltip" data-placement="top" title="Proses: <?= $count_proses ?>">
-                                                <?= $count_proses ?>
-                                            </button>
-                                            <button class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Selesai: <?= $count ?>">
-                                                <?= $count ?>
-                                            </button>
-                                        </td>
+                                    <td>
+                                        <button class="btn btn-danger" data-toggle="tooltip" data-placement="top"
+                                            title="Pesanan Masuk: <?= $count_masuk ?>">
+                                            <?= $count_masuk ?>
+                                        </button>
+                                        <button class="btn btn-warning" data-toggle="tooltip" data-placement="top"
+                                            title="Konfirmasi: <?= $count_konfirmasi ?>">
+                                            <?= $count_konfirmasi ?>
+                                        </button>
+                                        <button class="btn btn-info" data-toggle="tooltip" data-placement="top"
+                                            title="Proses: <?= $count_proses ?>">
+                                            <?= $count_proses ?>
+                                        </button>
+                                        <button class="btn btn-success" data-toggle="tooltip" data-placement="top"
+                                            title="Selesai: <?= $count ?>">
+                                            <?= $count ?>
+                                        </button>
+                                    </td>
 
 
-                                        <td class="table-action">
-                                            <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $value->id_user ?>"><i class="align-middle" data-feather="edit-2"></i></button>
+                                    <td class="table-action">
+                                        <!-- <button class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $value->id_user ?>"><i class="align-middle" data-feather="edit-2"></i></button>
                                             <button class="btn btn-danger" data-toggle="modal" data-target="#hapus<?= $value->id_user ?>"><i class="align-middle" data-feather="trash"></i></button> -->
-                                            <a href="<?= base_url('Pemilik/cTransaksi/transaksi/' . $value->id_user); ?>" style="color: white;" class="btn btn-secondary"><i class="align-middle" data-feather="eye"></i></a>
-                                        </td>
-                                    </tr>
+                                        <a href="<?= base_url('Pemilik/cTransaksi/transaksi/' . $value->id_user); ?>"
+                                            style="color: white;" class="btn btn-secondary"><i class="align-middle"
+                                                data-feather="eye"></i></a>
+                                    </td>
+                                </tr>
                                 <?php
                                 }
                                 ?>
